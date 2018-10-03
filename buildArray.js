019395144c98703ef903666dc470982e6a711963
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-03 11:22:11 -0700

    fixed capitalization

--- buildArray.js
+++ buildArray.js
@@ -1,29 +1,29 @@
 'use strict';
-const MemoryClass = require ('./memory');
-const Memory = new MemoryClass();
+const memoryClass = require ('./memory');
+const memory = new memoryClass();
 
 class Array {
   constructor() {  //initialize private variables to be used within this class
     this.length = 0;  //length is 0
-    this.ptr = Memory.allocate(this.length);  //initially this is a pointer to zero blocks of memory
+    this.ptr = memory.allocate(this.length);  //initially this is a pointer to zero blocks of memory
 }
 
 //Pushing a new element to end of array
 push(value) {
   this._resize(this.length +1);  //resizes the array to create space for one more element
-  Memory.set(this.ptr + this.length, value); //sets the memory at the pointer plus the length to be equal to the value
+  memory.set(this.ptr + this.length, value); //sets the memory at the pointer plus the length to be equal to the value
   this.length++; //increments the length by one
 }
 
 //This _resize function will allocate a new larger chunk of memory, copy any existing values from the old to the new chunk, and free the old chunk.  Like having a kid and needing to move everything into a new house, release the old house for someone else to live in.
 _resize(size) {
   const oldPtr = this.ptr; //you now start thinking of your house as the old house because you need a new bigger house
-  this.ptr = Memory.allocate(size); //you are requesting that your real estate agent find you a new house of the correct size
+  this.ptr = memory.allocate(size); //you are requesting that your real estate agent find you a new house of the correct size
   if (this.ptr === null) {   //if there are no homes of your requested size available, agent says "we're out"
     throw new Error ('Out of memory');
   }
-  Memory.copy(this.ptr, oldPtr, this.length); //agent found you the perfect size home! Move everything from your old house, including new kid, into your new house, and it all just fits perfectly....don't have another kid though, or you have to move again
-  Memory.free(oldPtr); //release the new house, now available for someone else to move in
+  memory.copy(this.ptr, oldPtr, this.length); //agent found you the perfect size home! Move everything from your old house, including new kid, into your new house, and it all just fits perfectly....don't have another kid though, or you have to move again
+  memory.free(oldPtr); //release the new house, now available for someone else to move in
 }
 
 //This has linear run time complexity O(n) -- it is very time consuming every time you have a kid!
--- buildBetterArray.js
+++ buildBetterArray.js
@@ -1,12 +1,12 @@
 'use strict';
-const MemoryClass = require ('./memory');
-const Memory = new MemoryClass();
+const memoryClass = require ('./memory');
+const memory = new memoryClass();
 
 class Array {
   constructor() {
     this.length = 0;
     this._capacity = 0;  //now adding _capacity so when we resize we can reserve more space than we currently need...that way we can have more kids or buy more stuff in the future without having to move 
-    this.ptr = Memory.allocate(this.length);
+    this.ptr = memory.allocate(this.length);
   }
 
   //push an element to the end of the array, and reserve extra space
@@ -14,18 +14,18 @@ class Array {
     if (this.length >= this._capacity) {
       this._resize((this.length + 1) * Array.SIZE_RATIO);  //if we are out of extra space, resize like we would before, but multiply it by whatever size ratio we have chosen based on how our quickly our array might grow
     }
-    Memory.set(this.ptr + this.length, value); 
+    memory.set(this.ptr + this.length, value); 
     this.length++;  
   }
 
   _resize(size) {
     const oldPtr = this.ptr;
-    this.ptr = Memory.allocate(size);
+    this.ptr = memory.allocate(size);
     if (this.ptr === null) {
       throw new Error ('Out of memory');
     }
-    Memory.copy(this.ptr, oldPtr, this.length);
-    Memory.free(oldPtr);
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
     this._capacity = size; //when you call _resize, be sure to pass in the size you want with space to grow into (if you want 3 kids, buy a house that can hold all three kids if you don't want to move later), this size is your _capacity.
   }
 }
