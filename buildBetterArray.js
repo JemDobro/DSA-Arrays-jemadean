17a05b9ec73f099599c2bb369b76e187bd7f1d51
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-03 10:31:04 -0700

    arrays understood and  built, hooked up to memory, drills started

---
+++ buildBetterArray.js
@@ -0,0 +1,47 @@
+'use strict';
+const MemoryClass = require ('./memory');
+const Memory = new MemoryClass();
+
+class Array {
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;  //now adding _capacity so when we resize we can reserve more space than we currently need...that way we can have more kids or buy more stuff in the future without having to move 
+    this.ptr = Memory.allocate(this.length);
+  }
+
+  //push an element to the end of the array, and reserve extra space
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);  //if we are out of extra space, resize like we would before, but multiply it by whatever size ratio we have chosen based on how our quickly our array might grow
+    }
+    Memory.set(this.ptr + this.length, value); 
+    this.length++;  
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = Memory.allocate(size);
+    if (this.ptr === null) {
+      throw new Error ('Out of memory');
+    }
+    Memory.copy(this.ptr, oldPtr, this.length);
+    Memory.free(oldPtr);
+    this._capacity = size; //when you call _resize, be sure to pass in the size you want with space to grow into (if you want 3 kids, buy a house that can hold all three kids if you don't want to move later), this size is your _capacity.
+  }
+}
+
+Array.SIZE_RATIO = 3;  //choose our size ratio based on how quickly our array might grow
+
+function main() {
+  Array.SIZE_RATIO = 3;
+  let arr = new Array();
+  arr.push(3);
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+  console.log(arr);
+}
+
+main();
